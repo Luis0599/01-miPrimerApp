6d3188cafedda5e5e2129079c87957d5464f592e
Luis Alfredo <alfredoluis0599@gmail.com>
2022-02-04 14:27:00 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'miPrimerApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
